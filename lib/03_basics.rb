bd38482fd46a16bc639168b574b293b0cf7efeee
AdrienPeligry <adrien.peligry@essec.edu>
2019-01-14 15:59:21 +0100

    third push

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,16 +1,42 @@
 def who_is_bigger (a, b, c)
-if a != "" || b != "" || c != ""
-
-puts [a, b, c].max
-
+	
+if a == nil || b == nil || c == nil
+	return "nil detected"
+elsif a > b && a > c
+	return "a is bigger"
+elsif b > a && b > c
+	return "b is bigger"
+elsif c > a && c > b
+	return "c is bigger"
 else 
-	puts "nil detected"
-
 
 end
 
-	#puts "nil detected"
+end
+
+
+
+def reverse_upcase_noLTA (a)
+	a = a.reverse
+	a = a.upcase
+	a = a.delete("LTA") 
+puts a
+return a
 end 
 
+#reverse_upcase_noLTA("Tries this at Home, Kids")
 
+
+def array_42 (a)
+	a.include?(42)
+end
+
+
+def magic_array (a)
+	a.flatten!
+	a.sort!
+	a.map! {|a| a * 2 }
+	a.uniq! 
+	a.delete_if {|a| a % 3 == 0 }
+end 
 
