9052e7dc7b61c6540b86014b0bb33ddb697e7755
geoffrey03 <geoffrey.poirrier@essec.edu>
2019-01-15 08:56:23 +0100

    push final

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -11,19 +11,19 @@ def translate (a)
 		then b = a.delete("ch") + "chay"
 		puts b
 		return b
-	elsif a == "eat pie"
-		then words = a.split
-		words.each do |u|
-			if u == "eat"
-				then words = u + "ay"
-				puts words
-			else
-				words = u.delete("p") + "pay"
-				puts words
-				a = words.join(" ")
-				puts a
-			end
-		end
+	#elsif a == "eat pie"
+	#	then words = a.split
+	#	words.each do |u|
+	#		if u == "eat"
+	#			then words = u + "ay"
+	#			puts words
+	#		else
+	#			words = u.delete("p") + "pay"
+	#			puts words
+	#			a = words.join(" ")
+	#			puts a
+	#		end
+	#	end
 	end
 end
 
