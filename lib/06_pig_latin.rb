2d509026f96803f4af145f290a49fb23357026b6
Geoffrey03 <45860333+Geoffrey03@users.noreply.github.com>
2019-01-21 00:03:26 +0100

    Update 06_pig_latin.rb

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -28,3 +28,27 @@ def translate (a)
 end
 
 translate("eat pie")
+
+# Partie de code pour que tout marche d'un coup
+def translate(string)
+	sufix = ''
+	res = ''
+
+	word_a = string.split(' ')
+
+	word_a.each do |word|
+		sufix = ''
+		while !word.start_with?('a', 'e', 'i', 'o', 'u')
+			if word[0..1] == 'qu'
+				sufix += word[0..1]
+				word[0..1] = ''
+			else
+				sufix += word[0]
+				word[0] = ''
+			end
+		end
+		res += word + sufix + 'ay '
+	end
+	
+	res.rstrip
+end 
